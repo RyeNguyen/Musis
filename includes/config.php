6e97714b1f805d6a1916b9cbcc3093af14080971
RyeNguyen <48546888+RyeNguyen@users.noreply.github.com>
2020-12-05 12:06:08 +0700

    Commit to deploy

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,15 @@
 <?php
-    ob_start(); //Turn on output buffering
-    session_start(); //Enable the use of sessions
+ob_start(); //Turn on output buffering
+session_start(); //Enable the use of sessions
 
-    $timezone = date_default_timezone_set("Asia/Ho_Chi_Minh");
+$timezone = date_default_timezone_set("Asia/Ho_Chi_Minh");
 
-    $connection = mysqli_connect("localhost", "root", "0973642872mh", "music_site");
+//    $connection = mysqli_connect("localhost", "root", "0973642872mh", "music_site");
 
 //    $connection = mysqli_connect("sql12.freemysqlhosting.net", "sql12377917", "Jc8sznCsCg", "sql12377917");
 
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect: " . mysqli_connect_errno();
-    }
+$connection = mysqli_connect("remotemysql.com", "I6D69FohtO", "6o55QvE5Ct", "I6D69FohtO");
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
